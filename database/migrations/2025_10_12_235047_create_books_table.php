b7e15ce5ae6786cda7af011c9db9faac9eab9fa0
rzkyalbani <rzkyalbani@gmail.com>
2025-10-13 07:01:23 +0700

    feat: add migrations for books, categories, and settings tables

---
+++ database/migrations/2025_10_12_235047_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('author')->nullable();
+            $table->string('isbn')->nullable();
+            $table->integer('total_copies')->default(1);
+            $table->integer('available_copies')->default(1);
+            $table->string('file_path')->nullable(); // untuk e-book
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
