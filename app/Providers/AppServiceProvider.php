0108da044e26a13e2ac99b85d254a9c1366847c7
rzkyalbani <rzkyalbani@gmail.com>
2025-10-15 02:07:56 +0700

    Add Indonesian localization support for the application
    - Set application locale to Indonesian (id) in app.php
    - Create Indonesian language files for authentication, pagination, passwords, and validation with appropriate translations.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        Carbon::setLocale('id');
+        setlocale(LC_TIME, 'id_ID.UTF-8');
     }
 }
